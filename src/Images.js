71d7d64256c5af559f0d1b811569e4571975a22f
rangeenameenu <rangeenaa@gmail.com>
2021-09-30 23:04:30 +0530

    Implemented collage display

--- src/Images.js
+++ src/Images.js
@@ -7,17 +7,6 @@ class Images extends React.Component {
         super(props);
         this.state = {
             imageUrl: "",
-            imageUrlArray: [
-                "https://www.cdc.gov/healthypets/images/slider/dog-and-cat.jpg",
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSg-zeC5cHjByHQHKyOjkRYt1IroeHWU911w&usqp=CAU",
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtwDUT66rGcuOz3urrDnrpYFWn5dMoWTm-Yg&usqp=CAU",
-                "https://assets.globalwildlife.org/m/3b6039a74fd067ea/webimage-Tigrinia.jpg",
-                "https://i.pinimg.com/originals/05/fa/46/05fa46f1d2ed44354532b85324c607d3.jpg",                
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWkzSW8qQUE1hSGxYSBwRIeBkIY8KkFLgwgDH8QwckYE1_h0IGg4SyurpMbO9k1NGJnYA&usqp=CAU",
-                "https://cdn.statically.io/img/media.kitsu.io/manga/poster_images/40286/medium.jpg?1509353777",
-                "https://media.istockphoto.com/vectors/eagle-emblem-vector-id1055078314?k=20&m=1055078314&s=170667a&w=0&h=WyHyMB2jEyHcZY2XboPdVAfjdsRc4Kkhs-0QOYXjWa0=",
-                "https://thumbs.dreamstime.com/b/wellness-tree-logo-icon-design-template-vector-natural-127334115.jpg"
-            ],
             showModal: false,
             popImageUrl: "",
             photosArray: []
@@ -25,7 +14,7 @@ class Images extends React.Component {
     }
 
     componentDidMount(){
-        let access_token = "EAAsdP01K4ZBEBABDxywE3u42q7ytk4DfbopED4M1PapdPAqcOyJQT5UZCGnJNoUQMDByUtuFDAncOKXyRD25L3YDfQ5aQX9VZBufsBPrad5it3BmKyzOUNiItyqhEZCPOSjmhaeaSL5vsOhhyWweVhGNU3aHtOZBIY5GxyhC4BjHhXfYuXM2NofTWh1KZCFZBpmihH432tTVZA0Lkwl1fXsHGiXyJHlZAXlHqtnQX0Y5r9JO1GzNuF6gw";
+        let access_token = "EAAsdP01K4ZBEBAL18zqMWrxPuxeGfpMrkWbi84TDAJnXe9afHGzI5nZB5HeC2brQcVcnKeBR3Hv3YQiuFzWhIZCON5CyH8WJrhvZAGGZAstr71NedDR4N7qHAb7VXACpW8b3EnpgDw0sMNAQqUDYwElVrJz7vlW4gARrAILnl0D4tRUcK4qNkoJ9eTsXcmx3aHCDuJjqDtiy2Y7pc2fZClYb8AtUNc1VeKkoyt7k0MZC2BUlqts8yXa";
         let id = "982923679229219"
         let url='https://graph.facebook.com/'+id+'?fields=photos{picture,likes.summary(true),created_time}&access_token='+access_token; //resource URI with user ID and access token
         axios({
@@ -49,11 +38,10 @@ class Images extends React.Component {
         })
         .catch(response=>{ /* error handling */
             console.log(response);
-        }) 
+        })
     }
 
     render() {
-        let imageUrlArray = this.state.imageUrlArray;
         const images = this.state.photosArray.map((image, index) => {
             return(
                 <img
